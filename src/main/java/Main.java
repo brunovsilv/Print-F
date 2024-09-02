d19a1af917662661514ebdc9dbc5210395a6a04b
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-09-02 12:17:23 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "Bruno";
+    int dia = 21;
+    int mes = 9;
+    int ano = 2007;
+    double pi = 3.14159265359;
+    System.out.printf("o valor de pi e: %.2f ",pi );
+      
+      
+  }
+
+  
+}
